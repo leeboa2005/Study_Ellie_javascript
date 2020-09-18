29fcb4ed756155755b4f6d7f065dd1be1e254f83
leeboa2005 <71476841+leeboa2005@users.noreply.github.com>
2020-09-18 15:58:24 +0900

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,60 @@
+"use script";
+
+//Fetch the items from the JSON file
+// 데이타가 있는 제이슨파일을 불러옴
+function loadItems() {
+  return fetch("data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
+
+// Update the list with the given items
+// .clothes의 하위에 아이템을 각각의 string(문자)상태로 만들어줌
+function displayItems(items) {
+  const container = document.querySelector(".clothes");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+// Create HTML list item from to given data item
+// item은 .clothes 밑에 들어갈 반복되리 내용을 채워준다 ` 중복 컨텐츠내용 `;
+// item.imge, item.type, item.gender, item.size는 제이슨 파일에 저장되어 있음
+function createHTMLString(item) {
+  return `
+    <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item_thumbnalil">
+            <p class="item_description">
+                ${item.gender}, ${item.size}
+            </p>
+   </li>
+    `;
+}
+// 버튼 클릭 이벤트
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+  // 해당하지 않은 경우에는 빨리 끝냄
+
+  const filtered = items.filter((item) => item[key] === value);
+  console.log(filtered);
+  displayItems(filtered);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  // 이벤트 위임 사용
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
+//main
+loadItems()
+  .then((items) => {
+    displayItems(items);
+    setEventListeners(items);
+  })
+  .catch(console.log);
